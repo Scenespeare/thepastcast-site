0377335b2c52358f2ecfe9a7b813babb44d454e7
Scenespeare <jacobbartholin@gmail.com>
2025-05-21 17:19:52 +0200

    Opdates and instalations

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./assets/**/*.{html,js}" // kig også i undermapper, hvis du får flere filer
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
